a0421c84c04c67119c8ad6309c8fdd863fe6c55b
Cotisel Radu <radu-bogdan.cotisel@continental-corporation.com>
2022-04-12 15:46:42 +0300

    Startup bug fix

--- Sources/Services/ShelterBackendMicroservices/UserManagementMicroservice/Startup.cs
+++ Sources/Services/ShelterBackendMicroservices/UserManagementMicroservice/Startup.cs
@@ -26,6 +26,8 @@ namespace UserManagementMicroservice
       // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
       public void ConfigureServices(IServiceCollection services)
       {
+         services.AddRouting();
+
          services.AddDbContext<UserContext>(options =>
          {
             options.UseSqlServer(Configuration.GetConnectionString("Default"));
