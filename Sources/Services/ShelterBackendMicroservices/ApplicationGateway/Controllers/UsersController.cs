9ab370d68e93906cbe2997e44135d2140a7a0f6a
Cotisel Radu <radu-bogdan.cotisel@continental-corporation.com>
2022-04-14 16:59:46 +0300

    Application Gateway introduced

---
+++ Sources/Services/ShelterBackendMicroservices/ApplicationGateway/Controllers/UsersController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApplicationGateway.Controllers
+{
+   [Route("[controller]")]
+   [ApiController]
+   public class UsersController : ControllerBase
+   {
+      [HttpGet]
+      [Route("login")]
+      public string Login()
+      {
+         return "User is in";
+      }
+
+      [HttpPost]
+      [Route("signup")]
+      public string SignUp()
+      {
+         return "User sign up";
+      }
+
+      [HttpPost]
+      [Route("update")]
+      public string Update()
+      {
+         return "User is updated";
+      }
+   }
+}
