5838ce0dbd4e8017fc13ecd75e1a4a7f21ff0e4c
Simonesiss <simona.radulea@gmail.com>
2022-04-16 22:49:22 +0300

    Create filter buttons component

---
+++ Sources/Mobile/ShelterMobile/src/components/FilterButtons.js
@@ -0,0 +1,131 @@
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useState } from "react";
+
+import {
+    StayIcon,
+    ChildcareIcon,
+    FoodIcon,
+    MedicalIcon,
+} from "../../assets/icons";
+import { style } from "../resources/colors";
+
+const FilterButtons = () => {
+    const [stayIsActive, setStayIsActive] = useState(true);
+    const [childcareIsActive, setChildcareIsActive] = useState(true);
+    const [foodIsActive, setFoodIsActive] = useState(true);
+    const [medicalIsActive, setMedicalIsActive] = useState(true);
+
+    return (
+        <View style={styles.container}>
+            <TouchableOpacity
+                style={{
+                    ...styles.button,
+                    backgroundColor: stayIsActive ? style.darkGray : style.white,
+                    borderColor: style.darkGray,
+                }}
+                onPress={() => setStayIsActive(!stayIsActive)}
+            >
+                <StayIcon fill={stayIsActive ? style.white : style.darkGray} />
+                <Text
+                    style={[
+                        styles.label,
+                        stayIsActive ? styles.colorWhite : { color: style.darkGray },
+                    ]}
+                >
+                    Stay
+                </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                style={{
+                    ...styles.button,
+                    backgroundColor: childcareIsActive
+                        ? style.secondaryBlue
+                        : style.white,
+                    borderColor: style.secondaryBlue,
+                }}
+                onPress={() => setChildcareIsActive(!childcareIsActive)}
+            >
+                <ChildcareIcon
+                    fill={childcareIsActive ? style.white : style.secondaryBlue}
+                />
+                <Text
+                    style={[
+                        styles.label,
+                        childcareIsActive
+                            ? styles.colorWhite
+                            : { color: style.secondaryBlue },
+                    ]}
+                >
+                    Childcare
+                </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                style={{
+                    ...styles.button,
+                    backgroundColor: foodIsActive ? style.accentOrange : style.white,
+                    borderColor: style.accentOrange,
+                }}
+                onPress={() => setFoodIsActive(!foodIsActive)}
+            >
+                <FoodIcon fill={foodIsActive ? style.white : style.accentOrange} />
+                <Text
+                    style={[
+                        styles.label,
+                        foodIsActive ? styles.colorWhite : { color: style.accentOrange },
+                    ]}
+                >
+                    Food
+                </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+                style={{
+                    ...styles.button,
+                    backgroundColor: medicalIsActive ? style.accentGreen : style.white,
+                    borderColor: style.accentGreen,
+                }}
+                onPress={() => setMedicalIsActive(!medicalIsActive)}
+            >
+                <MedicalIcon fill={medicalIsActive ? style.white : style.accentGreen} />
+                <Text
+                    style={[
+                        styles.label,
+                        medicalIsActive ? styles.colorWhite : { color: style.accentGreen },
+                    ]}
+                >
+                    Medical
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
+};
+
+export default FilterButtons;
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "100%",
+        marginVertical: 12,
+    },
+    button: {
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        paddingVertical: 6,
+        paddingHorizontal: 10,
+        borderRadius: 16,
+        borderWidth: 1,
+    },
+    label: {
+        fontSize: 14,
+        paddingLeft: 5,
+    },
+    colorWhite: {
+        color: style.white,
+    },
+});
