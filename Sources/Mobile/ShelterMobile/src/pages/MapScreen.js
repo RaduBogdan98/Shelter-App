14e721fcccd0d2e767485e5891173fd58d306107
Simonesiss <simona.radulea@gmail.com>
2022-04-16 22:49:48 +0300

    Update map screen
    Add filter buttons

--- Sources/Mobile/ShelterMobile/src/pages/MapScreen.js
+++ Sources/Mobile/ShelterMobile/src/pages/MapScreen.js
@@ -1,51 +1,52 @@
-import { StyleSheet, Text, View, Dimensions } from 'react-native'
-import React from 'react'
-import MapView from 'react-native-maps';
+import { StyleSheet, View, Dimensions } from "react-native";
+import React from "react";
+import MapView from "react-native-maps";
 
-import { SearchBar } from '../components';
+import { SearchBar, FilterButtons } from "../components";
 import { style } from "../resources/colors";
 
 const MapScreen = () => {
     return (
-        <View
-            style={styles.container}
-        >
+        <View style={styles.container}>
             <View style={styles.header}>
                 <SearchBar onSubmit={(searchText) => console.log(searchText)} />
+                <FilterButtons />
             </View>
             <MapView
                 provider="google"
                 style={styles.map}
                 initialRegion={{
                     latitude: 45.7532,
-                    longitude: 21.2270,
+                    longitude: 21.227,
                     latitudeDelta: 0.0722,
                     longitudeDelta: 0.0321,
-                }} />
+                }}
+            />
         </View>
-    )
-}
+    );
+};
 
-export default MapScreen
+export default MapScreen;
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: style.white,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
+        alignItems: "center",
+        justifyContent: "flex-start",
     },
     header: {
-        backgroundColor: style.white,
         height: 150,
-        width: '100%',
+        width: "100%",
         zIndex: 2,
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 20
+        alignItems: "center",
+        justifyContent: "flex-end",
+        padding: 20,
+        paddingBottom: 0,
+        backgroundColor: style.white,
     },
     map: {
-        height: Dimensions.get('screen').height,
-        width: Dimensions.get('screen').width
-    }
-})
+        height: Dimensions.get("screen").height,
+        width: Dimensions.get("screen").width,
+    },
+});
