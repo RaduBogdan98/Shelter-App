4d6d67ac9db570b82ffc9a77fb6ccdd9acab1387
Simonesiss <simona.radulea@gmail.com>
2022-04-16 20:49:52 +0300

    Create Search Bar component

---
+++ Sources/Mobile/ShelterMobile/src/components/SearchBar.js
@@ -0,0 +1,50 @@
+import { StyleSheet, TextInput, View } from "react-native";
+import React, { useState } from "react";
+
+import { SearchIcon } from "../../assets/icons";
+import { style } from "../resources/colors";
+
+const SearchBar = (props) => {
+
+    const [searchText, setSearchText] = useState("");
+
+    const onChangeText = (searchText) => {
+        setSearchText(searchText);
+    }
+
+    return (
+        <View style={styles.container}>
+            <SearchIcon />
+            <TextInput
+                style={styles.input}
+                value={searchText}
+                placeholder="Search"
+                placeholderTextColor={style.black}
+                onChangeText={(searchText) => onChangeText(searchText)}
+                onSubmitEditing={() => props.onSubmit && props.onSubmit(searchText)}
+            />
+        </View>
+    );
+};
+
+export default SearchBar;
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderRadius: 16,
+        paddingLeft: 16,
+        backgroundColor: style.baseGray25,
+    },
+    input: {
+        flex: 1,
+        width: '100%',
+        padding: 10,
+        fontSize: 18,
+        color: style.black,
+    },
+    icon: {
+        padding: 8
+    }
+});
