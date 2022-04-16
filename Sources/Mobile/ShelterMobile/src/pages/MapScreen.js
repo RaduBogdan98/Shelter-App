25eb3f1dd235ba328fadcf868884709a8814cfe2
Simonesiss <simona.radulea@gmail.com>
2022-04-16 20:50:44 +0300

    Create Map screen

--- Sources/Mobile/ShelterMobile/src/pages/MapScreen.js
+++ Sources/Mobile/ShelterMobile/src/pages/MapScreen.js
@@ -1,12 +1,27 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Dimensions } from 'react-native'
 import React from 'react'
+import MapView from 'react-native-maps';
+
+import { SearchBar } from '../components';
+import { style } from "../resources/colors";
 
 const MapScreen = () => {
     return (
         <View
             style={styles.container}
         >
-            <Text>MapScreen</Text>
+            <View style={styles.header}>
+                <SearchBar onSubmit={(searchText) => console.log(searchText)} />
+            </View>
+            <MapView
+                provider="google"
+                style={styles.map}
+                initialRegion={{
+                    latitude: 45.7532,
+                    longitude: 21.2270,
+                    latitudeDelta: 0.0722,
+                    longitudeDelta: 0.0321,
+                }} />
         </View>
     )
 }
@@ -16,8 +31,21 @@ export default MapScreen
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
+        backgroundColor: style.white,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    header: {
+        backgroundColor: style.white,
+        height: 150,
+        width: '100%',
+        zIndex: 2,
         alignItems: 'center',
         justifyContent: 'center',
+        padding: 20
     },
+    map: {
+        height: Dimensions.get('screen').height,
+        width: Dimensions.get('screen').width
+    }
 })
