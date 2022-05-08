4351c1177bf27751dab9fea84d8e37b6d915e4f3
Simonesiss <simona.radulea@gmail.com>
2022-05-08 22:09:24 +0300

    Create Auth pages

---
+++ Sources/Mobile/ShelterMobile/src/pages/index.js
@@ -0,0 +1,19 @@
+import HomeScreen from "./HomeScreen";
+import FeedScreen from "./FeedScreen";
+import MapScreen from "./MapScreen";
+import ProfileScreen from "./ProfileScreen";
+import DetailsScreen from "./DetailsScreen";
+import LoginScreen from "./LoginScreen";
+import RegisterScreen from "./RegisterScreen";
+import ForgotPasswordScreen from "./ForgotPasswordScreen";
+
+export {
+    HomeScreen,
+    FeedScreen,
+    MapScreen,
+    ProfileScreen,
+    DetailsScreen,
+    LoginScreen,
+    RegisterScreen,
+    ForgotPasswordScreen,
+};
